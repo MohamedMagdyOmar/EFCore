1e2688325e13185035f7829fe175aa0054b51af0
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-03-16 00:39:14 -0400

    Inserting Simple Objects
    1- inserting simple objects.
    2- enable logging on console, to see generated sql commands by EF.

--- SamuraiApp/SomeUI/Program.cs
+++ SamuraiApp/SomeUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,26 @@ namespace SomeUI
     {
         static void Main(string[] args)
         {
+            InsertSamurai();
+            Console.ReadLine();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            using (var context = new SamuraiContext())
+            {
+                // below add command makes the "context" track the samurai object
+                context.Samurais.Add(samurai);
+
+                // internal workflow is as follows:
+                    //1- Examine each tracked object
+                    //2- Read state of object
+                    //3- workout sql commands(construct correct sql statements)
+                    //4- execute each sql command in the database
+                    //5- capture any returned data
+                context.SaveChanges();
+            }
         }
     }
 }
